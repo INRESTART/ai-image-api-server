a09901893a6466f5fd60e0db5650c35cace3c013
INRESTART <inrestart@yandex.ru>
2025-07-15 13:32:40 +0300

    Update generate.php

--- generate.php
+++ generate.php
@@ -1,7 +1,7 @@
 <?php
 
-$openai_key = $_ENV['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'];
-$leonardo_key = $_ENV['c70f6c43-36ee-4ef3-9d67-4a1524c73c78'];
+$openai_key = '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';
+$leonardo_key = 'c70f6c43-36ee-4ef3-9d67-4a1524c73c78;
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $input = trim($_POST['text'] ?? '');
